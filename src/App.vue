96855446ee2be3549c1ae9f54b8fb029a6a2995d
OHHONEY1 <1269075501@qq.com>
2017-05-22 20:02:03 +0800

    添加项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <shape-shifter :content="content" :width="width" :height="height"></shape-shifter>
+  </div>
+</template>
+
+<script>
+import shapeShifter from "./components/ShapeShifter.vue"
+
+export default {
+  components: { shapeShifter },
+  data () {
+    return {
+      content: "hello|shape-shifter|#countdown 3|",
+      width: 1200,
+      height: 600
+    }
+  }
+}
+</script>
---
+++ src/components/ShapeShifter.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="shape-shifter">
+		<canvas class="canvas"></canvas>
+	</div>
+</template>
+
+<script>
+	import ShapeShifter from "../utils/shape-shifter.js"
+
+	export default {
+		name: 'vue-shape-shifter',
+		props: {
+			content: {
+				type: String,
+				default: 'hello shape-shifter'
+			},
+			width: {
+                type: Number,
+                default: 1000
+            },
+            height: {
+                type: Number,
+                default: 600
+            }
+		},
+		mounted () {
+            ShapeShifter(this.content, this.width, this.height)
+        }
+	}
+</script>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    #shape-shifter {
+    	width: 100%;
+        height: 100%;
+        text-align: center;
+        line-height: 100%;
+        background-color: #69b4ca;
+        /*background: -webkit-linear-gradient(top, rgb(203, 235, 219) 0%, rgb(55, 148, 192) 120%);
+        background: -moz-linear-gradient(top, rgb(203, 235, 219) 0%, rgb(55, 148, 192) 120%);
+        background: -o-linear-gradient(top, rgb(203, 235, 219) 0%, rgb(55, 148, 192) 120%);
+        background: -ms-linear-gradient(top, rgb(203, 235, 219) 0%, rgb(55, 148, 192) 120%);
+        background: linear-gradient(top, rgb(203, 235, 219) 0%, rgb(55, 148, 192) 120%);*/
+    }
+</style>
